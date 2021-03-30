1cf820d8a1c907a88e10cb6de6f867896704d7e5
Ahmad N. Primordi <ahmadnuftahprimordi@gmail.com>
2021-03-30 12:07:42 +0700

    implement score

---
+++ app/src/main/java/com/makaryostudio/mukbang/ui/score/ScoreFragment.kt
@@ -0,0 +1,32 @@
+package com.makaryostudio.mukbang.ui.score
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.makaryostudio.mukbang.R
+
+class ScoreFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ScoreFragment()
+    }
+
+    private lateinit var viewModel: ScoreViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.score_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ScoreViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/makaryostudio/mukbang/ui/score/ScoreViewModel.kt
@@ -0,0 +1,32 @@
+package com.makaryostudio.mukbang.ui.score
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.makaryostudio.mukbang.model.section.SectionData
+
+class ScoreViewModel : ViewModel() {
+    private val _cubeScore = MutableLiveData<Int>()
+    private val _prismScore = MutableLiveData<Int>()
+    private val _pyramidScore = MutableLiveData<Int>()
+    private val _evaluationScore = MutableLiveData<Int>()
+
+    val cubeScore: LiveData<Int>
+        get() = _cubeScore
+    val prismScore: LiveData<Int>
+        get() = _prismScore
+    val pyramidScore: LiveData<Int>
+        get() = _pyramidScore
+    val evaluationScore: LiveData<Int>
+        get() = _evaluationScore
+
+    private val listSection = SectionData.listSection
+
+    init {
+        // TODO: 30/03/2021 add score function 
+        _cubeScore.value = listSection[0].score
+        _prismScore.value = listSection[1].score
+        _pyramidScore.value = listSection[2].score
+        _evaluationScore.value = listSection[3].score
+    }
+}
